9759a70acfcca74d0a6a5ca90b3c41e2f7d657b1
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-03-18 12:32:01 +0400

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Sergey Hrabrov on 18.03.2023.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
